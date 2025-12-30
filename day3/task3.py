b22d956a24ef493cadec18f108afb63d392c2b3f
manto187 <asadameen050@gmail.com>
2025-12-31 04:55:01 +0500

    add logic to add number

--- day3/task3.py
+++ day3/task3.py
@@ -3,7 +3,7 @@ def add(a,b):
 def main():
     num1 = int(input("enter first number: "))
     num2 = int(input("enter second number: "))
-    print(add(5,5))
+    print("The sum is:", add(num1, num2))
 
 if __name__ == "__main__":
     main()
