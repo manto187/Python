35a665ece0ff67085fe992a16016dae5790ac010
manto187 <asadameen050@gmail.com>
2026-01-12 04:29:02 +0500

    file handling(reading from file)

---
+++ day4/task3_fileHandling.py
@@ -0,0 +1,14 @@
+import os
+def main():
+    filename = "sample.txt"
+
+    if os.path.exists(filename):
+
+        with open(filename, 'r') as file:
+            content = file.read()
+            print("file content:\n")
+            print(content)
+    else:
+            print("file does not exists")
+if __name__ == "__main__":
+     main()   
