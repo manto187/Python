e4849679179c2b9d6ded8b578da4e751ee582205
manto187 <asadameen050@gmail.com>
2026-04-02 18:21:49 +0500

    create labels for  series

--- sample2.py
+++ sample2.py
@@ -1,6 +1,15 @@
+# creating a series from a list
 import pandas as pd
 
 a=[1,2,3,4,5]
 
 myvar = pd.Series(a)
+print(myvar)
+
+# creating labels for the series
+import pandas as pd
+a=[1,2,3,4,5]
+
+myvar = pd.Series(a, index = ["a", "b", "c", "d","e"])
+
 print(myvar)
