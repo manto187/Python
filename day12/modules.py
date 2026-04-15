46ef566dde44ce2c1137d4a30b7a63d30b3e4342
manto187 <asadameen050@gmail.com>
2026-04-15 22:39:41 +0500

    basics of modules

---
+++ day12/modules.py
@@ -0,0 +1,14 @@
+# importing modules
+
+import math 
+print(math.sqrt(16))
+
+# import specific functions
+
+from math import sqrt
+print(sqrt(25))
+
+# importing with alias
+
+import math as m 
+print(m.pi)
