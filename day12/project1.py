f226858f2827d9fec2c5907d5cc532b396c044ea
manto187 <asadameen050@gmail.com>
2026-04-15 22:53:42 +0500

    random password generator

---
+++ day12/project1.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    password = ""
+    for i in range(length):
+        password += random.choice(characters)
+    
+    return password
+
+# User input
+length = int(input("Enter password length: "))
+
+password = generate_password(length)
+
+print("Generated Password:", password)
