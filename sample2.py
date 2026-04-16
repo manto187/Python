49af12446fe725ab439a3e735097a0caab7a2b44
manto187 <asadameen050@gmail.com>
2026-04-16 22:04:33 +0500

    dataframes basics

--- sample2.py
+++ sample2.py
@@ -31,3 +31,15 @@ calories = {"day1": 420, "day2": 380, "day3": 390}
 
 myvar = pd.Series(calories, index = ["day1", "day3"])
 print(myvar)
+
+
+#  dataframes 
+
+import pandas as pd
+data = {
+    "calories": [420, 380, 390],
+    "duration": [50, 40, 45]
+}
+
+myvar = pd.DataFrame(data)
+print(myvar)
