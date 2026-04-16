8c6205b4d760f0559dc8d57595f167bc12c52d48
manto187 <asadameen050@gmail.com>
2026-04-16 22:00:04 +0500

    key/value objects creation in Series

--- sample2.py
+++ sample2.py
@@ -13,3 +13,21 @@ a=[1,2,3,4,5]
 myvar = pd.Series(a, index = ["a", "b", "c", "d","e"])
 
 print(myvar)
+
+# we can use a key/value object, like dictionary when creating a series
+
+import pandas as pd
+calories = {"day1": 420, "day2": 380, "day3": 390}
+
+myvar = pd.Series(calories)
+print(myvar)
+
+
+# to select any some of items in dictionary use index arguemt and specify only the item
+# you want to include in the series
+
+import pandas as pd
+calories = {"day1": 420, "day2": 380, "day3": 390}
+
+myvar = pd.Series(calories, index = ["day1", "day3"])
+print(myvar)
