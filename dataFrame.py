af131cdeb5ac284a83b7031c78d0de70d0379044
manto187 <asadameen050@gmail.com>
2026-04-16 22:22:36 +0500

    using index argument and load files into datafram

--- dataFrame.py
+++ dataFrame.py
@@ -27,3 +27,36 @@ data = {
 }
 df = pd.DataFrame(data)
 print(df.loc[[0,1]])
+
+
+# named indexes
+
+import pandas as pd
+
+data = {
+    "calories":[420,380,390],
+    "duration":[50,40,45]
+}
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+print(df)
+
+
+# locating named indexes
+
+import pandas as pd
+
+data = {
+    "calories":[420,380,390],
+    "duration":[50,40,45]
+}
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+print(df.loc["day2"])
+
+
+# loading files into a dataframe
+
+import pandas as pd
+df = pd.read_csv("data.csv")
+print(df)
