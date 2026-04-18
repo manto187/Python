cdb421ca8f9ccc9282701f33f58623b32dd868eb
manto187 <asadameen050@gmail.com>
2026-04-18 18:37:20 +0500

    using markers and formating them

---
+++ markers.py
@@ -0,0 +1,18 @@
+# using marker keyword to emphasize each point with a marker
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o')
+plt.show()
+
+# format strings
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, 'o:r')
+plt.show()
