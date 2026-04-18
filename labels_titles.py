3910b7570a8d598cb1480278f0e013cb84be40f4
manto187 <asadameen050@gmail.com>
2026-04-18 23:50:56 +0500

    creating labels and titles for a plot

---
+++ labels_titles.py
@@ -0,0 +1,31 @@
+# with pyplot we can add labels to the x and y axis
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.xlabel("average pulse")
+plt.ylabel("calorie burange")
+
+plt.show()
+
+# creating title for a plot
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.plot(x, y)
+
+plt.title("sports watch data")
+plt.xlabel("average pulse")
+plt.ylabel("calorie burange")
+
+plt.show()
