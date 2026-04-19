c6b44595b19cd3bc79a016a22a82a16144f963b1
manto187 <asadameen050@gmail.com>
2026-04-19 11:05:45 +0500

    displaying multiple plots and adding title to each plot

---
+++ subplots.py
@@ -0,0 +1,64 @@
+# displaying multiple plots
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+# plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x, y)
+
+# plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(x, y)
+
+plt.show()
+
+
+#  two plots on top of each other
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+# plot 1 
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(2, 1, 1)
+plt.plot(x, y)
+
+# plot 2 
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(2, 1, 2)
+plt.plot(x, y)
+
+plt.show()
+
+# adding title to each plot
+
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+# plot 1
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1, 2, 1)
+plt.plot(x, y)
+plt.title("sales")
+
+# plot 2
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1, 2, 2)
+plt.plot(x, y)
+plt.title("income")
+
+plt.show()
