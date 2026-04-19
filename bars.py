05794fc646def7e61f33fd6bea4f3bd5bf92f6f6
manto187 <asadameen050@gmail.com>
2026-04-19 12:01:00 +0500

    bars width and height

--- bars.py
+++ bars.py
@@ -35,3 +35,26 @@ plt.bar(x, y, color = "#4CAF50")
 plt.show()
 
 
+# bar width
+
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, width = 0.1)
+
+plt.show()
+
+# bar height
+
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.barh(x, y, height = 0.1)
+
+plt.show()
