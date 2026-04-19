0e955c46db23f8b4b217e0992e4ebe44cd1c2c0b
manto187 <asadameen050@gmail.com>
2026-04-19 12:56:10 +0500

    exploding, shadowing and coloring the pie chart

--- pie_chart.py
+++ pie_chart.py
@@ -29,3 +29,42 @@ mylabels = ["apples", "bananas", "cherries", "dates"]
 
 plt.pie(y, labels = mylabels, startangle = 90)
 plt.show()
+
+
+# exploding the pie chart
+
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["apples", "bananas", "cherries", "dates"]
+myexplode = [0.2, 0, 0, 0]
+
+plt.pie(y, labels = mylabels, explode = myexplode)
+plt.show()
+
+
+# adding shadown to pie chart
+
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["apples", "bananas", "cherries", "dates"]
+myexplode = [0.2, 0, 0, 0]
+
+plt.pie(y, labels = mylabels, explode = myexplode, shadow = True)
+plt.show()
+
+
+# set color of each wedge with colors paramter
+
+import matplotlib.pyplot as plt 
+import numpy as np 
+ 
+y = np.array([35, 25, 25, 15])
+mylabels = ["apples", "bananas", "cherries", "dates"]
+mycolors = ["black", "hotpink", "b", "#4CAF50"]
+
+plt.pie(y, labels = mylabels, colors = mycolors)
+plt.show()
