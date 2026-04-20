2623d419f30d56c459c61dbdb2358090f8ef9564
manto187 <asadameen050@gmail.com>
2026-04-21 01:01:09 +0500

    set_context() to override default parameters

--- seaborn_week/figures1.py
+++ seaborn_week/figures1.py
@@ -41,3 +41,33 @@ import matplotlib.pyplot as plt
 tips = sns.load_dataset('tips')
 sns.lmplot(x ='total_bill', y ='tip', size = 2, aspect = 4, data = tips)
 plt.show()
+
+
+# scale and context 
+# using poster 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+# using poster 
+tips = sns.load_dataset('tips')
+sns.set_context('poster', font_scale = 2)
+sns.countplot(x='sex', data=tips, palette='coolwarm')
+plt.show()
+
+# using paper 
+tips = sns.load_dataset('tips')
+sns.set_context('paper', font_scale = 2)
+sns.countplot(x='sex', data=tips, palette='coolwarm')
+plt.show()
+
+# using notebook
+tips = sns.load_dataset('tips')
+sns.set_context('notebook', font_scale = 2)
+sns.countplot(x='sex', data=tips, palette='coolwarm')
+plt.show()
+
+# using talk 
+tips = sns.load_dataset('tips')
+sns.set_context('talk', font_scale = 2)
+sns.countplot(x='sex', data=tips, palette='coolwarm')
+plt.show()
