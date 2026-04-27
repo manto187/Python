42f579b20fc538fb25c1ed15d1dae1ee1ef4a2e2
manto187 <asadameen050@gmail.com>
2026-04-27 23:12:08 +0500

    finding element using beautiful soup

--- day19/parsing.py
+++ day19/parsing.py
@@ -6,3 +6,16 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.txt, "html.parser")
 print(soup.title.text)
+
+
+# find one heading 
+soup.find("h1")
+
+# find all
+soup.find_all("p")
+
+# by class 
+soup.find("div", class_="content")
+
+# by id 
+soup.find(id="main")
