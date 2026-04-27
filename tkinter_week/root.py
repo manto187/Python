fe0a2d3dccf604b3794e66e2e52d1b12a56675ed
manto187 <asadameen050@gmail.com>
2026-04-27 23:56:22 +0500

    label in root window and button

--- tkinter_week/root.py
+++ tkinter_week/root.py
@@ -5,3 +5,39 @@ root.title("hello world")
 root.geometry('350x200')
 
 root.mainloop()
+
+
+# label inside root window
+
+# from tkinter import *
+
+# root = Tk()
+
+# root.title("weclome to python")
+# root.geometry('350x200')
+
+# label = Label(root, txt = "are you a developer?")
+# label.grid()
+# root.mainloop()
+
+
+
+# adding button
+from tkinter import *
+
+root = Tk()
+root.title("weclome to python")
+root.geometry('350x200')
+
+label = Label(root, text = "are you a developer?")
+label.grid()
+
+def clicked():
+    label.configure(text = "im just a developer")
+
+btn = Button(root, text = "hire me", 
+             fg = "blue", command = clicked)
+
+btn.grid(column = 1, row=0)
+
+root.mainloop()
