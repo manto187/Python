95d4bf1c73f9b960fd2927b509f9fd29c28c7c87
manto187 <asadameen050@gmail.com>
2026-04-29 22:46:13 +0500

    created button without using TK themed widget

--- tkinter_week/buttons.py
+++ tkinter_week/buttons.py
@@ -31,3 +31,20 @@ button = tk.Button(root,
 button.pack(padx=20, pady=20)
 
 root.mainloop()
+
+
+
+# creation of buttons without using TK themed widget
+
+from tkinter import *         
+from tkinter.ttk import *
+
+root = Tk()           
+root.geometry('100x100')     
+
+btn = Button(root, text = 'Click me !', 
+                command = root.destroy) 
+
+btn.pack(side = 'top')     
+
+root.mainloop()
