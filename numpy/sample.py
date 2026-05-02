e6e2a3bb294454c8f8780a66298808df4c9eceb7
manto187 <asadameen050@gmail.com>
2026-05-02 20:47:31 +0500

    initializing  numpy arrays using special functions

--- numpy/sample.py
+++ numpy/sample.py
@@ -2,3 +2,19 @@ import numpy as np
 
 arr = np.array([1,2,3,4,5])
 print(arr)
+
+# NumPy array using special functions 
+
+import numpy as np 
+
+a0 = np.zeros((2, 3))
+a1 = np.ones((3, 3))
+af = np.full((2, 2), 7)
+ar = np.arange(0, 10, 2)
+la = np.linspace(0, 1, 5)
+
+print("zero array: ", "\n", a0)
+print("one array: ", "\n", a1)
+print("constant array: ", "\n", af)
+print("range array: ", "\n", ar)
+print("linspace array: ", "\n", la)
