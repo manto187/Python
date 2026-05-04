82772ec1ae081b0d1c5e56672a6434d16f4fa64c
manto187 <asadameen050@gmail.com>
2026-05-04 21:19:12 +0500

    resizing arrays

--- numpy/reshape_resize.py
+++ numpy/reshape_resize.py
@@ -16,3 +16,23 @@ import numpy as np
 arr = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 r = arr.reshape(3, -1)
 print(r)
+
+
+# resizing 1D array of 6 elements into a 2x3 array using np.resize()
+import numpy as np 
+arr = np.array([1,2,3,4,5,6])
+arr.resize((2,3))
+print(arr)
+
+
+# resizing 6 elements array into 3x4 array using np.resize() in-place
+import numpy as np 
+arr = np.array([1,2,3,4,5,6])
+arr.resize((3,4))
+print(arr)  
+
+# resizing array into a 2x2 shape
+import numpy as np 
+arr = np.array([1,2,3,4])
+arr.resize((2,2))
+print(arr)
