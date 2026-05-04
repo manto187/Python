ee6c70a52047135a43a516e547c1e9c6fc3a3295
manto187 <asadameen050@gmail.com>
2026-05-04 20:50:41 +0500

    created 3D array by grouping and use -1 to infer dimension

--- numpy/reshape_resize.py
+++ numpy/reshape_resize.py
@@ -4,3 +4,15 @@ arr = np.array([1,2,3,4,5,6])
 r = arr.reshape(2,3)
 print(r)
 
+# creating 3D array by grouping original elements 
+import numpy as np 
+arr = np.array(1,2,3,4,5,6,7,8)
+r = arr.reshape(2,2,2)
+print(r)
+
+
+# using -1 to infer dimensions
+import numpy as np 
+arr = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+r = arr.reshape(3, -1)
+print(r)
