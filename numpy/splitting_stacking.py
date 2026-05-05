082ce9efae436061d6c0041468ec2a0900511d8e
manto187 <asadameen050@gmail.com>
2026-05-05 21:15:46 +0500

    stacking along axis

--- numpy/splitting_stacking.py
+++ numpy/splitting_stacking.py
@@ -5,3 +5,13 @@ arr2 = np.array([4,5,6])
 
 result = np.stack((arr1, arr2), axis=0)
 print(result)
+
+
+# stacking same 1D array along axis 0, 1, -1 changing the output 
+import numpy as np 
+arr1 = np.array([1,2,3])
+arr2 = np.array([4,5,6])
+
+print(np.stack((arr1, arr2), axis=0))
+print(np.stack((arr1, arr2), axis=1))
+print(np.stack((arr1, arr2), axis=-1))
