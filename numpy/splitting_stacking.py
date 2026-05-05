6c4a405357e84234a7a591beee49441949e07dc9
manto187 <asadameen050@gmail.com>
2026-05-05 21:27:57 +0500

    stacking 2D arrays to new 3D structure

--- numpy/splitting_stacking.py
+++ numpy/splitting_stacking.py
@@ -15,3 +15,16 @@ arr2 = np.array([4,5,6])
 print(np.stack((arr1, arr2), axis=0))
 print(np.stack((arr1, arr2), axis=1))
 print(np.stack((arr1, arr2), axis=-1))
+
+
+
+# stacking two 2D arrays along axis 0,1,2 to show how the new 3D structure changes
+import numpy as np 
+x = np.array([[1,2,3],
+              [4,5,6]])
+y = np.array([[7,8,9],
+              [10,11,12]])
+
+print(np.stack((x,y), axis=0))
+print(np.stack((x,y), axis=1))
+print(np.stack((x,y), axis=2))
