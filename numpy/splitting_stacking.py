016d7d685a025e2eeac6fa2a74915c314700aa3a
manto187 <asadameen050@gmail.com>
2026-05-05 21:32:57 +0500

    stacking two 3D arrays

--- numpy/splitting_stacking.py
+++ numpy/splitting_stacking.py
@@ -28,3 +28,15 @@ y = np.array([[7,8,9],
 print(np.stack((x,y), axis=0))
 print(np.stack((x,y), axis=1))
 print(np.stack((x,y), axis=2))
+
+
+
+# stacking two 3D arrays along axis 0,1,2 to demonstrate how stacking works with higher dimension data
+import numpy as np 
+m = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+n = np.array([[[10,20], [30,40],[50, 60], [70, 80]]])
+
+print(np.stack((m,n), axis=0))
+print(np.stack((m,n), axis=1))
+print(np.stack((m,n), axis=2))
+print(np.stack((m,n), axis=3))
