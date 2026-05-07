5458e8ded3d8f1ab0203428dbeaee08e1404de3d
manto187 <asadameen050@gmail.com>
2026-05-07 17:03:34 +0500

    basic arithmetic operations

---
+++ numpy/mathematical_operations/basic_op.py
@@ -0,0 +1,37 @@
+# addition of arrays
+import numpy as np 
+a = np.array([1,2,3])
+b = np.array([2,3,4])
+result = np.add(a,b)
+print(result)
+
+# subtraction of arrays
+import numpy as np 
+a = np.array([1,2,3])
+b = np.array([2,3,4])
+result = np.subtract(a,b)
+print(result)
+
+
+# division of arrays 
+import numpy as np 
+a = np.array([1,2,3])
+b = np.array([2,3,4])
+result = np.divide(a,b)
+print(result)
+
+
+# exponentiation
+import numpy as np 
+a = np.array([1,2,3])
+b = np.array([2,3,4])
+result = np.power(a,b)
+print(result)
+
+
+# modulus operation
+import numpy as np 
+a = np.array([1,2,3])
+b = np.array([2,3,4])
+result = np.mod(a,b)
+print(result)
