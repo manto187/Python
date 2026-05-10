de2e4871c10527035162d38528a12cfeb3d3da3f
manto187 <asadameen050@gmail.com>
2026-05-10 22:54:10 +0500

    min() function for arrays

--- numpy/mathematical_operations/min_max.py
+++ numpy/mathematical_operations/min_max.py
@@ -22,3 +22,21 @@ import numpy as np
 a = np.array([[1,4,7], [2,5,8]])
 b = np.array([3,3,3])
 print(np.maximum(a,b))
+
+
+import numpy as np 
+a = 10 
+b = 21
+print(np.minimum(a,b))
+
+
+import numpy as np 
+a = np.array([2,8,123])
+b = np.array([3,3,15])
+print(np.minimum(a,b))
+
+
+import numpy as np 
+a = np.array([np.nan, 0, np.nan])
+b = np.array([np.nan, 0, np.nan])
+print(np.minimum(a, b))
