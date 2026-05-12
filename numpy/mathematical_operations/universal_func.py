c40bfa036859d72b3322432485ea6d3c8f6d374b
manto187 <asadameen050@gmail.com>
2026-05-12 22:47:29 +0500

    statistical calculations on an array of students

--- numpy/mathematical_operations/universal_func.py
+++ numpy/mathematical_operations/universal_func.py
@@ -13,3 +13,29 @@ print("hyperbolic sine: ", sinh_vals)
 
 hyp = np.hypot(3, 4)
 print("hypotenuse: ", hyp)
+
+
+
+import numpy as np 
+weights = np.array([150.7, 52.5, 50, 58, 55.63, 73.25, 49.5, 45])
+
+# min and max 
+print("min and max", np.amin(weights), np.amax(weights))
+
+# range
+print("range: ", np.ptp(weights))
+
+# 70th percentile 
+print("70th percentile: ", np.percentile(weights, 70))
+
+# mean
+print("mean: ", np.mean(weights))
+
+# standard deviation
+print("standard deviation: ", np.std(weights))
+
+# variance
+print("variance: ", np.var(weights))
+
+# average
+print("average: ", np.average(weights))
