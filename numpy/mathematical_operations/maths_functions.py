1facd0df2f6f90c6e10732cfcc266d3833216065
manto187 <asadameen050@gmail.com>
2026-05-14 22:57:23 +0500

    numpy mathematical functions(expo, log, sine, hyperbolic etc)

---
+++ numpy/mathematical_operations/maths_functions.py
@@ -0,0 +1,33 @@
+import numpy as np 
+arr = [0, np.pi/2, np.pi/3, np.pi]
+s1 = np.sin(arr)
+print("sine values:\n", s1)
+
+
+# example 2
+import numpy as np 
+arr = [0, np.pi/2, np.pi/3, np.pi]
+s1 = np.cos(arr)
+print("Cosine values:\n", s1)
+
+# example 3
+import numpy as np 
+arr = [0, np.pi/2, np.pi/3, np.pi]
+s1 = np.sinh(arr)
+print("hyperbolic sine values:\n", s1)
+
+# example 4
+import numpy as np 
+arr = [1.2, 2.7, -3.4, -4.6]
+print(np.rint(arr))
+
+
+# example 5
+import numpy as np 
+arr = [1,3,5]
+print(np.exp(arr))
+
+# example 6
+import numpy as np 
+arr = [1,3,5,256]
+print(np.log(arr))
