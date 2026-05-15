57f6a0223d49de0706d5482f715411d1d3813eac
manto187 <asadameen050@gmail.com>
2026-05-15 23:08:58 +0500

    element-wise multiplication vs matrix multiplication

--- numpy/linear_algebra/manipulation.py
+++ numpy/linear_algebra/manipulation.py
@@ -6,3 +6,11 @@ print("addition\n:", np.add(x,y))
 print("subtraction\n:", np.subtract(x,y))
 print("division\n:", np.divide(x,y))
 
+
+# element wise multiplication vs matrix multiplication
+import numpy as np 
+x = np.array([[1,2], [4,5]])
+y = np.array([[7,8], [9,10]])
+
+print("element wise multiplication:\n ", np.multiply(x,y))
+print("matrix multiplication:\n ", np.dot(x,y))
