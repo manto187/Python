c67ec34c3cbfa22fb63e29063b2a7c8e3add4e17
manto187 <asadameen050@gmail.com>
2026-05-15 23:56:30 +0500

    outer product of vectors

--- numpy/linear_algebra/matrix_vector_products.py
+++ numpy/linear_algebra/matrix_vector_products.py
@@ -12,3 +12,21 @@ y = np.array([[1,5,0], [5,10,3]])
 
 print("inner product of matrices x and y =")
 print(np.inner(x,y))
+
+
+# outer product 
+import numpy as np 
+
+# define vectors
+a = np.array([2,6])
+b = np.array([3,10])
+
+print("outer product of a and b = ")
+print(np.outer(a,b))
+
+# define matrices
+x = np.array([[3,6,4], [9,4,6]])
+y = np.array([[1,15,7], [3,10,8]])
+
+print("outer product of matrices x and y =")
+print(np.outer(x,y))
