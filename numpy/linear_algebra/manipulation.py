419dee0fe9825011602f506e3554b31f9d5323e7
manto187 <asadameen050@gmail.com>
2026-05-15 23:05:43 +0500

    element-wise addition, subtraction and division

---
+++ numpy/linear_algebra/manipulation.py
@@ -0,0 +1,8 @@
+import numpy as np 
+x = np.array([[1,2], [4,5]])
+y = np.array([[7,8], [9,10]])
+
+print("addition\n:", np.add(x,y))
+print("subtraction\n:", np.subtract(x,y))
+print("division\n:", np.divide(x,y))
+
