1802ef08955aa11a96e5260d845265562a09fc22
manto187 <asadameen050@gmail.com>
2026-05-15 20:26:01 +0500

    multiplying a  3x2 matrix with a 2x3 matrix

--- numpy/linear_algebra/matrix_multiplication.py
+++ numpy/linear_algebra/matrix_multiplication.py
@@ -10,3 +10,18 @@ print(b)
 c = np.dot(a, b)
 print("result: ")
 print(c)
+
+
+# example 2
+import numpy as np 
+x = [[1,2], [2,3], [4,5]]
+y = [[4,5,1], [6,7,2]]
+
+print("matrix x: ")
+print(x)
+print("matrix y: ")
+print(y)
+
+result = np.dot(x, y)
+print("result:")
+print(result)
