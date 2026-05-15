6e7c0ceb32acf30cb03a7a281d3d6ddd2acb9e73
manto187 <asadameen050@gmail.com>
2026-05-15 23:52:29 +0500

    dot product(inner product) in numpy

---
+++ numpy/linear_algebra/matrix_vector_products.py
@@ -0,0 +1,14 @@
+# inner product 
+import numpy as np 
+a = np.array([2,6])
+b = np.array([3,10])
+
+print("inner product of vectors a and b =")
+print(np.inner(a,b))
+
+# define matrices
+x = np.array([[2,3,4],[3,2,9]])
+y = np.array([[1,5,0], [5,10,3]])
+
+print("inner product of matrices x and y =")
+print(np.inner(x,y))
