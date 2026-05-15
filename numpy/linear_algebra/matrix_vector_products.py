16ea04a3790ea80f775d0a44b13a1675a1cf6842
manto187 <asadameen050@gmail.com>
2026-05-15 23:58:22 +0500

    cross product of vectors(converted to 3D)

--- numpy/linear_algebra/matrix_vector_products.py
+++ numpy/linear_algebra/matrix_vector_products.py
@@ -30,3 +30,23 @@ y = np.array([[1,15,7], [3,10,8]])
 
 print("outer product of matrices x and y =")
 print(np.outer(x,y))
+
+
+
+
+# cross product
+import numpy as np
+
+# Define vectors as 3D (z=0 for 2D compatibility)
+a = np.array([3, 6, 0])
+b = np.array([9, 10, 0])
+
+print("Cross product of vectors a and b =")
+print(np.cross(a, b))
+
+# Define matrices (already 3D rows)
+x = np.array([[2, 6, 9], [2, 7, 3]])
+y = np.array([[7, 5, 6], [3, 12, 3]])
+
+print("Cross product of matrices x and y =")
+print(np.cross(x, y))
