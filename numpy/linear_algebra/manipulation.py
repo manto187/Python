48e45058a44b763a50c1f03cb61e2e168f350d60
manto187 <asadameen050@gmail.com>
2026-05-15 23:12:22 +0500

    matrix operations( square root, sum, transpose)

--- numpy/linear_algebra/manipulation.py
+++ numpy/linear_algebra/manipulation.py
@@ -14,3 +14,16 @@ y = np.array([[7,8], [9,10]])
 
 print("element wise multiplication:\n ", np.multiply(x,y))
 print("matrix multiplication:\n ", np.dot(x,y))
+
+
+# other useful numpy matrix functions 
+import numpy as np
+x = np.array([[1,2], [4,5]])
+y = np.array([[7,8], 9,10])
+
+print("square root:\n", np.sqrt(x))
+print("sum of all elements:", np.sum(y))
+
+print("column-wise sum: ", np.sum(y, axis=0))
+print("row-wise sum:", np.sum(y, axis=1))
+print("transpose:\n", x.T)
