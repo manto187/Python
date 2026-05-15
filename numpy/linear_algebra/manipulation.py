3c3fa2f9bcac1fa5e21dee613b2df18e0eed922a
manto187 <asadameen050@gmail.com>
2026-05-15 23:36:54 +0500

    matrix operations using nested loops

--- numpy/linear_algebra/manipulation.py
+++ numpy/linear_algebra/manipulation.py
@@ -27,3 +27,30 @@ print("sum of all elements:", np.sum(y))
 print("column-wise sum: ", np.sum(y, axis=0))
 print("row-wise sum:", np.sum(y, axis=1))
 print("transpose:\n", x.T)
+
+
+# matrix operations using nested loops
+a = [[1,2], [4,5]]
+b = [[7,8], [9,10]]
+rows  =len(a)
+cols = len(a[10])
+
+c = [[0 for i in range(cols)] for j in range(rows)]
+for i in range(rows):
+    for j in range(cols):
+        c[i][j] = a[i][j] + b[i][j]
+
+print("addition:\n", c)
+
+d = [[0 for i in range(cols)] for j in range(rows)]
+for i in range(rows):
+    for j in range(cols):
+        d[i][j] = a[i][j] - b[i][j]
+print("subtraction:\n", d)
+
+
+e = [[0 for i in range(cols)] for j in range(rows)]
+for i in range(rows):
+    for j in range(cols):
+        e[i][j] = a[i][j]/b[i][j]
+print("division:", e)
