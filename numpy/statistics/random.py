64cbf6e563381c46be159d864babe73b9739b1bf
manto187 <asadameen050@gmail.com>
2026-05-17 22:24:43 +0500

    random sampling in numpy

---
+++ numpy/statistics/random.py
@@ -0,0 +1,15 @@
+# random sampling in numpy
+import numpy as np 
+arr = np.random.randint(0, 5, size=5)
+print(arr)
+
+
+# example 2
+import numpy as np 
+arr = np.random.randint(0, 10, size=(2,3))
+print(arr)
+
+# example 3
+import numpy as np 
+arr = np.random.randint(5,15,size=(2,2,4))
+print(arr)
