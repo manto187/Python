a8780a294c495b139765594b3de9fa3d1abcf09a
manto187 <asadameen050@gmail.com>
2026-05-17 23:11:28 +0500

    vistualizing normal distribution

--- numpy/statistics/normal_distribution.py
+++ numpy/statistics/normal_distribution.py
@@ -13,3 +13,26 @@ print(arr)
 import numpy as np 
 m = np.random.normal(loc=10, scale=2, size=(2,3))
 print(m)
+
+# visualisation of normal distribution  
+import numpy as np 
+import matplotlib.pyplot as plt 
+from scipy.stats import norm 
+
+# generate data 
+data = np.random.normal(loc=0, scale=1, size=1000)
+
+# plot histogram
+plt.hist(data, bins=30, edgecolor="black", density=True)
+
+# plot theoretical PDF
+x = np.linspace(-4, 4, 200)
+pdf = norm.pdf(x, loc=0, scale=1)
+plt.plot(x, pdf, label="theoretical PDF")
+
+plt.title("normal distribution")
+plt.xlable("value")
+plt.ylabel("density")
+plt.grid(True)
+plt.legend()
+plt.show()
