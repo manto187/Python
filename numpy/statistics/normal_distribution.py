22e9ccdbc772b1589d9fab3d21a4adcd5c3a9342
manto187 <asadameen050@gmail.com>
2026-05-17 23:10:22 +0500

    normal distribution

---
+++ numpy/statistics/normal_distribution.py
@@ -0,0 +1,15 @@
+import numpy as np 
+x = np.random.normal()
+print(x)
+
+
+# example 1 
+import numpy as np 
+arr = np.random.normal(size=5)
+print(arr)
+
+
+# example 2
+import numpy as np 
+m = np.random.normal(loc=10, scale=2, size=(2,3))
+print(m)
