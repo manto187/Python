91b7af74f3903f68a62cfac76e0f790da3370e49
manto187 <asadameen050@gmail.com>
2026-05-18 23:18:04 +0500

    binomial distribution

---
+++ numpy/statistics/binomial_distribution.py
@@ -0,0 +1,13 @@
+import numpy as np 
+x = np.random.binomial(n=10, p=0.5)
+print(x)
+
+# example 1
+import numpy as np 
+arr = np.random.binomial(n=10, p=0.5, size=5)
+print(arr)
+
+# example 2 
+import numpy as np 
+x = np.random.binomial(12, 0.6, size=(2,3))
+print(x)
