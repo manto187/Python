778e2f196f8d4db0d4e044eb946bf4c766379ede
manto187 <asadameen050@gmail.com>
2026-05-19 23:45:25 +0500

    adding two arrays element-wise

--- numpy/advance_op/vectorization.py
+++ numpy/advance_op/vectorization.py
@@ -5,4 +5,9 @@ num= 2
 result = a1+num
 print(result)
 
-# adding
+# adding two arrays element wise 
+import numpy as np 
+a1 = np.array([1,2,3])
+a2 = np.array([4,5,6])
+result = a1+a2
+print(result)
