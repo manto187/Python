4f277ebe2cb0e0f1a0a20252e4fdf540a4d63bbd
manto187 <asadameen050@gmail.com>
2026-05-26 00:06:24 +0500

    add scatter and line plots for tips dataset visualization with markers for meal times

---
+++ seaborn_week/relational_plots/relational_plot2.py
@@ -0,0 +1,41 @@
+import seaborn as sns
+
+from matplotlib_week import markers 
+sns.set(style="ticks")
+
+tips = sns.load_dataset('tips')
+sns.relplot(x="total_bill", y="tips", data=tips)
+
+# using relplot() with kind="scatter" to create a scatter plot of total bill vs tip amount in the tips dataset
+import seaborn as sns
+sns.set(style ="ticks") 
+tips = sns.load_dataset('tips')
+
+sns.relplot(x ="total_bill",
+            y ="tip",
+            kind ="scatter", 
+            data = tips)
+
+# using relplot() with kind="line" to create a line plot of total bill vs tip amount in the tips dataset
+import seaborn as sns   
+sns.set(style="ticks")
+tips = sns.load_dataset('tips')
+
+sns.relplot(x="total_bill",
+            y="tips",
+            kind="line",
+            data=tips)
+
+
+# plotting a scatterplot using  marker to differentiate between timing of the people visiting the restaurant
+import seaborn as sns 
+
+sns.set(style="ticks")
+tips = sns.load_dataset('tips')
+markers = {"Lunch": "o", "Dinner": "s"}
+sns.relplot(x="total_bill",
+            y="tip",
+            hue="time",
+            markers=markers,
+            data=tips)
+
