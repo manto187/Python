f646df0d32d218a3db779701e6a0868bacf7d1cc
manto187 <asadameen050@gmail.com>
2026-05-30 00:48:06 +0500

    add histogram visualization with hue for penguin species and tips dataset

--- seaborn_week/distribution_plots/histogram.py
+++ seaborn_week/distribution_plots/histogram.py
@@ -15,3 +15,15 @@ import seaborn as sns
 penguins = sns.load_dataset("penguins")
 
 sns.histplot(data = penguins, x = "body_mass_g", kde=True)
+
+# plot histogram with hue to compare distribution of body mass across different penguin species
+sns.histplot(data = penguins, x = "body_mass_g", kde=True, hue="species")
+
+# 3.
+import numpy as np 
+import pandas as pd 
+import seaborn as sns 
+
+tips = sns.load_dataset("tips")
+
+sns.histplot(data=tips, x="size", stat="probability", discrete=True)
