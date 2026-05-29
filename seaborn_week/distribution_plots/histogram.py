bea2fe332fb300b2efdf16acf291840a7178644d
manto187 <asadameen050@gmail.com>
2026-05-29 23:24:17 +0500

    add histogram visualization for penguin species dataset

--- seaborn_week/distribution_plots/histogram.py
+++ seaborn_week/distribution_plots/histogram.py
@@ -6,3 +6,12 @@ np.random.seed(1)
 num_var = np.random.randn(1000)
 num_var = pd.Series(num_var,name="numerical variable")
 sns.histplot(data = num_var, kde = True)
+
+# 2. dataset showing characteristics of different penguin species
+import numpy as np 
+import pandas as pd 
+import seaborn as sns 
+
+penguins = sns.load_dataset("penguins")
+
+sns.histplot(data = penguins, x = "body_mass_g", kde=True)
