217c81f0197aa81670ec6b2416414dd89448b87d
manto187 <asadameen050@gmail.com>
2026-05-30 01:19:03 +0500

    add additional examples for jointplot visualizations

--- seaborn_week/distribution_plots/jointplot.py
+++ seaborn_week/distribution_plots/jointplot.py
@@ -8,3 +8,21 @@ sns.jointplot(x="solutions", y="score",
               kind = "hex", data = data)
 plt.show()
 
+# example 2 
+import seaborn as sns 
+import matplotlib as plt 
+
+data = sns.load_dataset("mpg")
+sns.jointplot(x="mpg", y="acceleration", 
+              kind = "scatter", data = data)
+plt.show()
+
+
+# example 3 
+import seaborn as sns 
+import matplotlib as plt 
+data = sns.load_dataset("exercise")
+
+sns.jointplot(x="id", y="pulse", 
+              kind = "kde", data = data)
+plt.show()
