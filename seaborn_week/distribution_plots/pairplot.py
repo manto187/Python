207e79533296a4577e118f6d7a90074d74324893
manto187 <asadameen050@gmail.com>
2026-05-30 12:11:34 +0500

    pairplot with specific variables

--- seaborn_week/distribution_plots/pairplot.py
+++ seaborn_week/distribution_plots/pairplot.py
@@ -13,3 +13,10 @@ df = seaborn.load_dataset('tips')
 custom_palette = {'Male': 'lightblue', 'Female': 'pink'}
 seaborn.pairplot(df, hue='sex', palette=custom_palette)
 plt.show()
+
+# example 3 
+import seaborn 
+import matplotlib.pyplot as plt 
+df = seaborn.load_dataset('tips')
+seaborn.pairplot(df[['total_bill', 'tips', 'size']])
+plt.show()
