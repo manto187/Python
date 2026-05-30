708906a9235f81295ee7d1efe9c0472f1d1280b0
manto187 <asadameen050@gmail.com>
2026-05-30 12:09:56 +0500

    pairplot with custom palette by sex

--- seaborn_week/distribution_plots/pairplot.py
+++ seaborn_week/distribution_plots/pairplot.py
@@ -4,3 +4,12 @@ import matplotlib.pyplot as plt
 df = seaborn.load_dataset('tips')
 seaborn.pairplot(df, hue='day')
 plt.show()
+
+
+# example 2 
+import seaborn 
+import matplotlib.pyplot as plt 
+df = seaborn.load_dataset('tips')
+custom_palette = {'Male': 'lightblue', 'Female': 'pink'}
+seaborn.pairplot(df, hue='sex', palette=custom_palette)
+plt.show()
--- seaborn_week/distribution_plots/tempCodeRunnerFile.py
+++ seaborn_week/distribution_plots/tempCodeRunnerFile.py
@@ -1,5 +1,6 @@
 import seaborn 
 import matplotlib.pyplot as plt 
 df = seaborn.load_dataset('tips')
-seaborn.pairplot(df, hue='day')
+custom_palette = {'Male': 'lightblue', 'Female': 'pink'}
+seaborn.pairplot(df, hue='sex', palette=custom_palette)
 plt.show()
