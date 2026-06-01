1827b67cfcc82522dc8899fdbc0683040a257c3b
manto187 <asadameen050@gmail.com>
2026-06-01 23:57:37 +0500

    set of vertical bars with nested grouping by two variables

--- seaborn_week/categorical_plots/barplot.py
+++ seaborn_week/categorical_plots/barplot.py
@@ -3,3 +3,11 @@ import matplotlib.pyplot as plt
 df = sns.load_dataset('titanic')
 sns.barplot(x='class', y='fare', data=df)
 plt.show()
+
+
+# example 2
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+df = sns.load_dataset('titanic')
+sns.barplot(x='class', y='fare', hue='sex', data=df)
+plt.show()
