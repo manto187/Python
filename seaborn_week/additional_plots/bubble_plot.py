2b8868083283ba702e4773846cc0c130344d8c1a
manto187 <asadameen050@gmail.com>
2026-06-03 13:19:41 +0500

    scatter plot with seaborn

--- seaborn_week/additional_plots/bubble_plot.py
+++ seaborn_week/additional_plots/bubble_plot.py
@@ -6,3 +6,13 @@ data = "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba
 
 df = pd.read_csv(data)
 df.head()
+
+# depict scatterplot illustration
+sns.set_context("talk", font_scale=1.1)
+plt.figure(figsize=(8,6))
+sns.scatterplot(x="sepal.length",
+                y="sepal.width",
+                data=df)
+
+plt.xlabel("Sepal.Length")
+plt.ylabel("sepal.width")
