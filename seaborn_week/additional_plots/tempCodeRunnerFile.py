066eca6469fe1343926dda1bc94217d8733adb1b
manto187 <asadameen050@gmail.com>
2026-06-03 13:16:04 +0500

    iris data as a pandas data frame

---
+++ seaborn_week/additional_plots/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import seaborn as sns 
+
+data = "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"
+
+df = pd.read_csv(data)
+df.head()
