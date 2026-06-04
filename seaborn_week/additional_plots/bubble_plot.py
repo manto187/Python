6d1f55c1589a7620b119ac2589657415b5593430
manto187 <asadameen050@gmail.com>
2026-06-04 23:18:53 +0500

    add bubble plot with explicit size ranges and color differantiation using seaborn

--- seaborn_week/additional_plots/bubble_plot.py
+++ seaborn_week/additional_plots/bubble_plot.py
@@ -30,3 +30,36 @@ plt.ylabel("petal.width")
 plt.tight_layout()
 plt.savefig("Bubble_plot_Seaborn_scatterplot.png",
             format='png', dpi=150)
+
+
+
+# bubble plot with explicit size ranges seaborn scatterplot()
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import seaborn as sns 
+
+data = "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"
+
+df = pd.read_csv(data)
+
+df.head()
+
+sns.set_context("talk", font_scale=1.2)
+plt.figure(figsize=(10,6))
+sns.scatterplot(x='sepal.length',
+                y='sepal.width',
+                sizes=(20,500),
+                alpha=0.5,
+                hue='variety',
+                data=df)
+
+plt.legend(bbox_to_anchor=(1.01,1),
+borderaxespad=0)
+
+plt.xlabel("sepal.length")
+plt.ylabel("sepal.width")
+plt.title("bubble plot with colors in seaborn")
+
+plt.tight_layout()
+
+                
