f8b3a01a65ae148cfdd7145595b6405b743e01ce
manto187 <asadameen050@gmail.com>
2026-06-05 21:07:04 +0500

    residual plot for iris dataset

--- seaborn_week/additional_plots/resideplot.py
+++ seaborn_week/additional_plots/resideplot.py
@@ -7,3 +7,15 @@ sns.residplot(x="total_bill",
               data=data)
 
 plt.show()
+
+
+# example 2 
+import seaborn as sns 
+import matplotlib.pyplot as plt
+data = sns.load_dataset("iris")
+
+sns.residplot(x="petal_length",
+              y="petal_width",
+              data=data,
+              lowess=True)
+plt.show()
