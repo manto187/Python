bd45caa8584fcc5a69b8b7a0400b1027250dfac8
manto187 <asadameen050@gmail.com>
2026-06-06 21:51:26 +0500

    add additional point plot example with custom markers and linestyle for tips dataset

--- seaborn_week/additional_plots/point_plot.py
+++ seaborn_week/additional_plots/point_plot.py
@@ -19,3 +19,17 @@ sns.pointplot(x="sex",
               hue="smoker",
               data=data)
 plt.show()
+
+# example 3 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+data = sns.load_dataset("tips")
+
+sns.pointplot(x="day",
+                y="total_bill",
+                linestyle='-.',
+                markers='^',
+                hue="sex",
+                data=data)
+plt.show()
