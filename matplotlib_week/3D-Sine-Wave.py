3124ae312ed2d49cc5b5b45305abcebb39b61ffc
manto187 <asadameen050@gmail.com>
2026-06-09 00:07:15 +0500

    plotted a simple 3D line graph of sine wave signal

--- matplotlib_week/3D-Sine-Wave.py
+++ matplotlib_week/3D-Sine-Wave.py
@@ -13,3 +13,20 @@ ax = plt.axes(projection = '3d')
 
 ax.scatter(x, y, z, c=c)
 plt.show()
+
+
+# example 2 
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure(figsize = (8, 8))
+ax = plt.axes(projection = '3d')
+
+z = np.linspace(0, 15, 1000)
+x = np.sin(z)
+y = np.cos(z)
+zline = z
+
+ax.plot3D(x, y, zline, 'gray')
+plt.show()
