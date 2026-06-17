891be0a7d4f60351c738bb3f4895a6c6500cda03
manto187 <asadameen050@gmail.com>
2026-06-17 23:16:41 +0500

    barplot tasks using matplotlib

--- seaborn_week/categorical_plots/barplot.py
+++ seaborn_week/categorical_plots/barplot.py
@@ -11,3 +11,46 @@ import matplotlib.pyplot as plt
 df = sns.load_dataset('titanic')
 sns.barplot(x='class', y='fare', hue='sex', data=df)
 plt.show()
+
+
+# example 3 
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+df = sns.load_dataset('titanic')
+sns.barplot(x='fare', y='class', hue='sex', data=df)
+plt.show()
+
+# example 4 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+df = sns.load_dataset('titanic')
+sns.barplot(x='class', y='fare', data=df,
+            order=["Third", "Second", "First"])
+plt.show()
+
+# example 5 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+df = sns.load_dataset('titanic')
+sns.barplot(x='class', y='fare', data=df, color="salmon")
+plt.show()
+
+
+# example 6 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+df = sns.load_dataset('titanic')
+sns.barplot(x='class', y='fare', data=df, ci=None)
+plt.show()
+
+# example 7 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+df = sns.load_dataset('titanic')
+sns.barplot(x='class', y='fare', data=df, hue='sex', palette='pastel')
+plt.show()
