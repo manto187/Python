f1d511c731a059a0485c049b38db4e3c6d52f777
manto187 <asadameen050@gmail.com>
2026-06-18 23:48:15 +0500

    horizantal boxplot and boxplot with hue

--- seaborn_week/categorical_plots/boxplot.py
+++ seaborn_week/categorical_plots/boxplot.py
@@ -5,3 +5,18 @@ df = sns.load_dataset("tips")
 sns.boxplot(x="day", y="tips", data=df)
 plt.show()
 
+# example 1 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+df = sns.load_dataset("tips")
+sns.boxplot(x=df["total_bill"])
+plt.show()
+
+# example 2 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+df = sns.load_dataset("tips")
+sns.boxplot(x="day", y="total_bill", hue="smoker", data=df)
+plt.show()
