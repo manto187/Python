d5479964b81780978a34170dca78196069bfe65b
manto187 <asadameen050@gmail.com>
2026-06-23 21:51:03 +0500

    strip plot using jitter parameter

--- seaborn_week/categorical_plots/stripplot.py
+++ seaborn_week/categorical_plots/stripplot.py
@@ -14,3 +14,8 @@ tips = sns.load_dataset("tips")
 sns.stripplot(x=tips["total_bill"])
 plt.show()
 
+# example 2 
+sns.set(style='whitegrid')
+tips = sns.load_dataset("tips")
+sns.stripplot(x="day", y="total_bill", data=tips, jitter=0.1)
+plt.show()
