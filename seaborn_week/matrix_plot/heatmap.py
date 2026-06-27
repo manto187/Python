ba5105bd45eca75a79d8749c853a5228d6c91131
manto187 <asadameen050@gmail.com>
2026-06-27 23:52:29 +0500

    advance customization of heatmap

--- seaborn_week/matrix_plot/heatmap.py
+++ seaborn_week/matrix_plot/heatmap.py
@@ -33,3 +33,26 @@ import matplotlib.pyplot as plt
 data = np.random.randint(1,100,(10,10))
 sns.heatmap(data, cmap='coolwarm', center=50)
 plt.show()
+
+# advance customizations in seaborn heatmap
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import matplotlib.colors as mcolors
+
+data=np.random.rand(10,10) *100
+plt.figure(figsize=(12,8))
+
+sns.heatmap(
+    data,
+    xticklabels=list("ABCDEFGHIJ"),
+    yticklabels=False,
+    norm=mcolors.LogNorm(),
+    cmap="coolwarm",
+    linewidths=0.5
+)
+
+plt.title("custom heatmap", fontsize=16)
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.show()
