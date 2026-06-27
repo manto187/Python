33a53cbc07bf1f892921d9e8e1979628d532c694
manto187 <asadameen050@gmail.com>
2026-06-27 23:48:39 +0500

    used parameter such as vmin,vmax,cmap,center for better visualization of heatmap using seaborn

--- seaborn_week/matrix_plot/heatmap.py
+++ seaborn_week/matrix_plot/heatmap.py
@@ -5,3 +5,31 @@ import matplotlib.pyplot as plt
 data = np.random.randint(1,100,(10,10))
 sns.heatmap(data)
 plt.show()
+
+# example 1 
+import numpy as np 
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+data = np.random.randint(1,100,(10,10))
+sns.heatmap(data, vmin=30, vmax=70)
+plt.show()
+
+
+# example 2 
+import numpy as np 
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+data = np.random.randint(1,100,(10,10))
+sns.heatmap(data, cmap='tab20')
+plt.show()
+
+# example 3 
+import numpy as np 
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+data = np.random.randint(1,100,(10,10))
+sns.heatmap(data, cmap='coolwarm', center=50)
+plt.show()
