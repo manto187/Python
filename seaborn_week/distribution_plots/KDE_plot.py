c7d0a645fecc8542a9413c08a910960b6d7c1b5c
manto187 <asadameen050@gmail.com>
2026-06-27 23:39:56 +0500

    bivariate kdeplot for two variables of iris

--- seaborn_week/distribution_plots/KDE_plot.py
+++ seaborn_week/distribution_plots/KDE_plot.py
@@ -14,3 +14,16 @@ sns.kdeplot(x,y)
 sns.kdeplot(x,y,shade=True)
 sns.kdeplot(x,y,cmap="winter_r")
 sns.kdeplot(x,y, shade=True, cbar=True)
+
+# for iris dataset
+iris = sns.load_dataset("iris")
+iris
+setosa=iris.loc[iris.species=="setosa"]
+viriginica=iris.loc[iris.species=="viriginica"]
+sns.kdeplot(setosa.petal_length, setosa.petal_width)
+
+sns.kdeplot(setosa.sepal_width, setosa.sepal_length)
+
+# two separate kdeplots with different variables 
+sns.kdeplot(setosa.petal_length, setosa.petal_width)
+sns.kdeplot(viriginica.petal_length, viriginica.petal_width)
