660f0d404138feff95a3776117c889f2ad5f249d
manto187 <asadameen050@gmail.com>
2026-07-25 23:56:50 +0500

    add correlation heatmap for NASA exoplanet dataset

--- seaborn_week/matrix_plot/correlation_heatmap.py
+++ seaborn_week/matrix_plot/correlation_heatmap.py
@@ -11,3 +11,14 @@ print(co_mtx)
 plt.title("Correlation Heatmap")
 sns.heatmap(co_mtx, cmap="YlGnBu", annot=True)
 plt.show()
+
+# correlation heatmap for NASA exoplanet dataset
+import matplotlib.pyplot as mp
+import pandas as pd
+import seaborn as sb
+
+data = pd.read_csv("exoplanets.csv")
+
+dataplot = sb.heatmap(data.corr(numeric_only=True))
+
+mp.show()
