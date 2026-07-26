6f12ffd896e83a71a6f025022d2f20f011ce6e93
manto187 <asadameen050@gmail.com>
2026-07-26 23:50:42 +0500

    triangle correlation heatmap for exoplanet space research dataset

--- seaborn_week/matrix_plot/triangle_correlation.py
+++ seaborn_week/matrix_plot/triangle_correlation.py
@@ -11,3 +11,18 @@ dataplot = sns.heatmap(data.corr(), cmap="YlGnBu",
 annot=True, mask=mask)
 
 plt.show()
+
+
+# example 2 
+import matplotlib.pyplot as mp
+import pandas as pd
+import seaborn as sb
+import numpy as np 
+
+data = pd.read_csv("exoplanets.csv")
+
+mask = np.triu(np.ones_like(data.corr()))
+
+dataplot = sb.heatmap(data.corr(), mask=mask)
+
+mp.show()
