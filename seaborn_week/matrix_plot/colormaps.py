366019866a2cb50bf330c67392c05cdb0b33ea38
manto187 <asadameen050@gmail.com>
2026-07-27 23:56:55 +0500

    add diverging colormaps and modified greens

--- seaborn_week/matrix_plot/colormaps.py
+++ seaborn_week/matrix_plot/colormaps.py
@@ -8,3 +8,26 @@ data = np.random.rand(12,12)
 colormap = sns.color_palette("Greens")
 
 ax = sns.heatmap(data, cmap=colormap)
+
+# 2
+import seaborn as sns
+import numpy as np
+
+
+np.random.seed(0)
+
+data = np.random.rand(12, 12)
+ax = sns.heatmap(data, cmap="Greens")
+
+# 3
+sns.palplot(sns.color_palette("PiYG", 12))
+sns.palplot(sns.color_palette("coolwarm", 12))
+
+import seaborn as sns
+import numpy as np
+
+
+np.random.seed(0)
+
+data = np.random.rand(12, 12)
+ax = sns.heatmap(data, cmap="PiYG")
