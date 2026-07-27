2f4299e9dc93b6ef61ea35c75a1466b6c28c7891
manto187 <asadameen050@gmail.com>
2026-07-28 00:08:15 +0500

    add frames to seaborn heatmap with axhline and axvline

--- seaborn_week/matrix_plot/frames.py
+++ seaborn_week/matrix_plot/frames.py
@@ -10,3 +10,24 @@ example = example.pivot("month", "year",
 res = sns.heatmap(example)
 plt.show()
 
+# using axhline and axvline 
+
+# example 1 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+
+example = sns.load_dataset("flights")
+example = example.pivot("month", "year",
+                        "passengers")
+
+res = sns.heatmap(example, cmap="BuPu")
+
+res.axhline(y=0, color='k', linewidth=10)
+res.axhline(y=example.shape[1], color='k',
+            linewidth=10)
+res.axvline(x=0, color='k', 
+            linewidth=10)
+res.axvline(x=example.shape[0],
+            color='k', linewidth=10)
+
+plt.show()
