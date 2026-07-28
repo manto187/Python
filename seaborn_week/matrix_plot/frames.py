0e290cd8cd8cd4bc15dd6cf65c0a2841a8bef831
manto187 <asadameen050@gmail.com>
2026-07-28 23:55:55 +0500

    add example of seaborn heatmap with random data and enhanced frame lines

--- seaborn_week/matrix_plot/frames.py
+++ seaborn_week/matrix_plot/frames.py
@@ -31,3 +31,20 @@ res.axvline(x=example.shape[0],
             color='k', linewidth=10)
 
 plt.show()
+
+# example 2 
+# Import libraries
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+example = np.random.rand(10, 12)
+res = sns.heatmap(example, cmap = "magma",linewidths = 0.5)
+res.axhline(y = 0, color = 'k',linewidth = 15)
+
+res.axhline(y = 10, color = 'k',linewidth = 15)
+
+res.axvline(x = 0, color = 'k',linewidth = 15)
+
+res.axvline(x = 12, color = 'k',linewidth = 15)
+plt.show()
