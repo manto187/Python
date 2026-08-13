ad73d661c6e34eba2f26f37442459f552e9de2b1
manto187 <asadameen050@gmail.com>
2026-08-13 23:48:00 +0500

    basic visualization of "fmri" dataset using swarmplot()

---
+++ seaborn_week/categorical_plots/swarmplot.py
@@ -0,0 +1,8 @@
+import seaborn 
+
+seaborn.set(style='whitegrid')
+fmri = seaborn.load_dataset("fmri")
+
+seaborn.swarmplot(x="timepoint",
+                  y="signal",
+                  data=fmri)
