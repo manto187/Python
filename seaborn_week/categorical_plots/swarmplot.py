7546ab28debdc51296f3bc251bd7ec467088afb1
manto187 <asadameen050@gmail.com>
2026-08-13 23:48:47 +0500

    grouping data points on the basis of category(regoin and event)

--- seaborn_week/categorical_plots/swarmplot.py
+++ seaborn_week/categorical_plots/swarmplot.py
@@ -6,3 +6,14 @@ fmri = seaborn.load_dataset("fmri")
 seaborn.swarmplot(x="timepoint",
                   y="signal",
                   data=fmri)
+
+
+# example 2 
+import seaborn
+seaborn.set(style='whitegrid')
+fmri = seaborn.load_dataset("fmri")
+
+seaborn.swarmplot(x="timepoint",
+                  y="signal",
+                  hue="region",
+                  data=fmri)
