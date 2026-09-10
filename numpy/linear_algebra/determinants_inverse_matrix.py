6dd2117bb2222babe2bad6ba8680435f68a1ab0f
manto187 <asadameen050@gmail.com>
2026-09-10 23:35:29 +0500

    determinant using numpy.linalg.det() method

--- numpy/linear_algebra/determinants_inverse_matrix.py
+++ numpy/linear_algebra/determinants_inverse_matrix.py
@@ -4,3 +4,10 @@ A = np.array([[50, 29], [30,44]])
 sign, logdet = np.linalg.slogdet(A)
 result = sign * np.exp(logdet)
 print("determinant of A =", result)
+
+
+# example 2 
+import numpy as np 
+A = np.array([[1,2],[3,4]])
+result = np.linalg.det(A)
+print("determinant of A =", result)
